8e7a76fca9dbf95aee3919f54494334853f1c4ee
MaciejGrudziaz <maciejgrudziaz@gmail.com>
2019-11-13 23:31:08 +0100

    main cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    std::cout<<"double max: "<<std::numeric_limits<double>::max()<<std::endl;
-
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
